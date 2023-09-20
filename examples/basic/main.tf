78141b87d970e4ff96c36c71fd65aa3b7e9f9c4d
David Harris <11905011+dharrisio@users.noreply.github.com>
2023-09-20 10:04:05 -0600

    Update docs and module example usage (#3)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,7 +2,18 @@ module "tabular_bucket" {
   source = "../../"
 
   bucket_name        = var.bucket_name
-  external_id        = var.external_id
+  external_id        = var.organization_id
   tabular_account_id = var.tabular_account_id
   tabular_region     = var.tabular_region
+}
+
+resource "tabular_s3_storage_profile" "example" {
+  region         = var.tabular_region
+  s3_bucket_name = module.tabular_bucket.s3_bucket_name
+  role_arn       = module.tabular_bucket.iam_role_arn
+}
+
+resource "tabular_warehouse" "example" {
+  name            = "tabular-warehouse-name"
+  storage_profile = tabular_s3_storage_profile.example.id
 }
---
+++ examples/basic/providers.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    tabular = {
+      source = "tabular-io/tabular"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
